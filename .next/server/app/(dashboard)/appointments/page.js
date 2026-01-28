9461f342dae92e422300d01299ccb8a27a236191
Marcio Sager <marcioager@gmail.com>
2026-01-28 22:00:38 +0100

    Remove .next from git, add to gitignore - fix Turbopack/Webpack conflict

--- .next/server/app/(dashboard)/appointments/page.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/(dashboard)/appointments/page.js";
-const runtime = require("../../../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_064dfb._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__c1b2f6._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/(dashboard)/appointments/page/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-page.js?page=/(dashboard)/appointments/page { COMPONENT_0 => \"[project]/app/layout.tsx [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_1 => \"[project]/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_2 => \"[project]/app/(dashboard)/layout.tsx [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_3 => \"[project]/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_4 => \"[project]/app/(dashboard)/appointments/page.tsx [app-rsc] (ecmascript, Next.js server component)\" } [app-rsc] (ecmascript) <facade>", CHUNK_PUBLIC_PATH).exports;
