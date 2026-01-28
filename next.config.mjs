6c95e168ac725aef599d865e73ead263b53a603e
Marcio Sager <marcioager@gmail.com>
2026-01-28 20:59:49 +0100

    Add standalone output for Docker deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
-  outputFileTracingRoot: process.cwd()
 }
 
 export default nextConfig
